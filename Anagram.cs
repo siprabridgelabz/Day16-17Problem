6eea5823a9ce37675a7fe99e56f775f3bbf18a2c
sipra <sipramishra085@gmail.com>
2023-03-29 22:48:19 +0530

    Added Algorithm problem 6

---
+++ Anagram.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTreeDemo
+{
+     public  class Anagram
+    {
+        public static void CheckAnagram()
+        {
+            string str1 = "heater";
+            string str2 = "reheat";
+            char[] ch1 = str1.ToLower().ToCharArray();
+            char[] ch2 = str2.ToLower().ToCharArray();
+
+            Array.Sort(ch1);
+            Array.Sort(ch2);
+
+            string val1 = new string(ch1);
+            string val2 = new string(ch2);
+
+            Console.WriteLine(val1);
+            Console.WriteLine(val2);
+
+            if( val1 == val2)
+            {
+                Console.WriteLine("It's an Anagram");
+
+            }
+            else
+            {
+                Console.WriteLine("It's not  an Anagram");
+
+            }
+
+
+
+        }
+    }
+}
