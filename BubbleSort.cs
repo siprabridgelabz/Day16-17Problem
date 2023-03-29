27acbbba114e0816131ec2ae0ee3c117985913c3
sipra <sipramishra085@gmail.com>
2023-03-29 22:45:13 +0530

    Added Algorithm problem 3

---
+++ BubbleSort.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTreeDemo
+{
+    public  class BubbleSort
+    {
+        public static void DisplayBubble()
+        {
+            int[] arr = {78,55,45,98,13 };
+            int temp;
+            for(int j=0;j<=arr.Length-2;j++)
+            {
+                for(int i=0;i<=arr.Length-2;i++)
+                {
+                    if (arr[i] > arr[i + 1]) ;
+                    {
+                        temp = arr[i + 1];
+                        arr[i + 1] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("Sorted: ");
+            foreach(int p in arr)
+            {
+                Console.WriteLine(p + " ");
+            }
+        }
+
+
+    }
+}
