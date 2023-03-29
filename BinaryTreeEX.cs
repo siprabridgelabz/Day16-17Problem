c908a009b680e1d9fba9d02d71aa8dc8e6413d25
sipra <sipramishra085@gmail.com>
2023-03-29 22:41:58 +0530

    Added Algorithm problem 1

---
+++ BinaryTreeEX.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace BinaryTreeDemo
+{
+    public class BinaryTreeEX
+    {
+        
+        public Node Root { get; set; }
+        public bool Add(int value)
+        {
+            Node before = null, after = this.Root;
+
+            while (after != null)
+            {
+                before = after;
+                if (value < after.Data)
+                {
+                    after = after.LeftNode;
+                }
+                else if (value > after.Data)
+                {
+                    after = after.RightNode;
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+            Node newnode = new Node();
+            newnode.Data = value;
+            if (this.Root == null)
+            {
+                this.Root = newnode;
+
+            }
+            else
+            {
+                if (value < before.Data)
+                {
+                    before.LeftNode = newnode;
+                }
+                else
+                {
+                    before.RightNode = newnode;
+                }
+            }
+            return true;
+
+        }
+        public void TravarsePreorder(Node Parent)
+        {
+            if (Parent != null)
+            {
+                Console.WriteLine(Parent.Data + " ");
+                TravarsePreorder(Parent.LeftNode);
+                TravarsePreorder(Parent.RightNode);
+            }
+        }
+    }
+}
---
+++ Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTreeDemo
+{
+    public  class Node
+    {
+        
+        public Node LeftNode { get; set; }
+        public Node RightNode { get; set; }
+
+        public int Data { get; set; }
+    }
+}
+
